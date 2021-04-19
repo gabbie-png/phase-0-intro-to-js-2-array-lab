d566789ae4efebff66d6520cbde3e301f225a4b8
Gabriella RoseMarino <Gabriellarosemarino@gmail.com>
2021-04-19 16:38:58 -0400

    commit tree 2e13390702d0d1b796bc47339789e86f1c2190dc

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your solution here!
+const cats = ['Milo', "Otis", "Garfield"];
+
+function destructivelyAppendCats(name){
+    var newArray = [...cats];
+    newArray.push(name);
+    return "Ralph"
+    
+}
+
+
+
